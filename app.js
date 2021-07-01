9dd7251404b30991539a5f2b986186702d99b6bf
Mishal <83138774+mishal284@users.noreply.github.com>
2021-07-01 14:06:25 +0500

    Updated grocery list app

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ function newElement() {
     var t = document.createTextNode(inputValue);
 
     li.appendChild(t);
-    if ("inputValue === ''") {
+    if (inputValue === '') {
         alert("You must write something!");
     } else {
         document.getElementById("myUL")
