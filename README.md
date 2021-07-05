9b0ec11b298c4d3824142c0070dbd47baf4a9df0
Mishal <83138774+mishal284@users.noreply.github.com>
2021-07-06 00:07:28 +0500

    updated readme

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
-# Grocery-list-App
-#Live Repo link:
+## Grocery-list-App
+-This repo is is for Javascript problem solving practice activity.
+-This is a group activity.
+## How this app work:
+-This App has funtionality to Add and clear items in grocery list.
+## Group Member:
+-Rabbia saleh
+-Arsalan khan
+-Mahhnoor Khan
+-Mishal Iftikhar
+-Khalil 
+## Technologies used:
+-HTML
+-CSS
+-JavaScript 
+## Software needed for code to run:
+-VS Code
+-Browser
+## live link:
 https://mishal284.github.io/Grocery-list-App/index.html
-#Repo link:
+## Repo link:
 https://github.com/mishal284/Grocery-list-App
--- app.js
+++ app.js
@@ -1,65 +1,51 @@
-const container = document.getElementById("myUL"); //get the list of groceries//
-var myNodelist = document.getElementsByclassName("li");
-
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-    var span = document.createElement("SPAN");
-
-    var txt = document.createTextNode("\u00D7");
-
-
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
+const container = document.getElementById("myUL"); //get the list of groceries//
+var myNodelist = document
+    .getElementsByTagName("li");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+}
+var close = document
+    .getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
 }
 
-var close = document.getElementsByClassName("close");
-var i;
-
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-    }
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document
+        .getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        document.getElementById("myUL")
+            .appendChild(li);
+    }
+    document.getElementById("myInput")
+        .value = "";
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
 }
-
-
-
-function newElement() {
-    var li = document.createElement("li");
-
-    var inputValue = document
-        .getElementById("input").value;
-
-    var t = document.createTextNode(inputValue);
-
-    li.appendChild(t);
-    if (inputValue === '') {
-        alert("You must write something!");
-    } else {
-        document.getElementById("myUL")
-            .appendChild(li);
-    }
-
-    document.getElementById("input")
-        .value = "";
-    var span = document.createElement("SPAN");
-
-    var txt = document.createTextNode("\u00D7");
-
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-            var div = this.parentElement;
-            div.style.display = "none";
-        }
-    }
-}
-
-// make a function that delet all the groceries list items when button is fired//
-function deletAll() {
-    container.innerHTML = "";
+// make a function that delet all the groceries list items when button is fired//
+function deletAll() {
+    container.innerHTML = "";
 }
